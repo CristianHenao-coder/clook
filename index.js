449eefcee0195cd7bdd4c5cb220a031207069624
santiagobot321 <boteroramirezsantiago@gmail.com>
2025-12-04 19:28:18 -0500

    despertar server

--- index.js
+++ index.js
@@ -281,5 +281,11 @@ app.get('/secret/:id', async (req, res) => {
     return res.redirect(model.onlyfans);
 });
 
+// Express
+app.get('/ping', (req, res) => {
+  res.status(200).send('pong');
+});
+
+
 // -------------------------------------------
 app.listen(port, () => console.log(`Server running on port ${port}`));
